1327f310efa7e7bca09d9fb3df958059efb8d202
vemm8ks2 <vemm8ks2@gmail.com>
2024-08-23 09:35:20 +0900

    Update T_shippingAddress.sql
    오타 정정 및 규칙에 맞게 수정합니다.

--- src/main/webapp/SQL/T_shippingAddress.sql
+++ src/main/webapp/SQL/T_shippingAddress.sql
@@ -1,10 +1,10 @@
 -- 배송지 shipping_address 테이블
-create table T_shippingAddress(
-	shippingAddress_id NUMBER(9),													-- 배송지식별자
-	shippingAddress_address VARCHAR2(100) not null,									-- 주소지
-	shippingAddress_id_default CHAR(1) not null,									-- 기본배송지 여부
-	shippingAddress_customer_Fk NUMBER(9),											-- 유저외래키
+create table T_shipping_address(
+	shipping_address_id NUMBER(9),													-- 배송지식별자
+	shipping_address_destination VARCHAR2(100) not null,									-- 주소지
+	shipping_address_is_default CHAR(1) not null,									-- 기본배송지 여부
+	shipping_address_customer_fk NUMBER(9),											-- 유저외래키
 	
-	primary key(shippingAddress_id),												-- 기본키
-	foreign key(shippingAddress_customer_Fk) references T_customer(customer_id)		-- 외래키
+	primary key(shipping_address_id),												-- 기본키
+	foreign key(shipping_address_customer_fk) references T_customer(customer_id)		-- 외래키
 );
