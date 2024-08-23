c80390c63375150ea7e987db423dcefec533dd1e
vemm8ks2 <vemm8ks2@gmail.com>
2024-08-23 10:10:44 +0900

    feature/39 테이블 제약 조건 수정 완료 및 추가 수정

--- src/main/webapp/SQL/T_admin.sql
+++ src/main/webapp/SQL/T_admin.sql
@@ -4,7 +4,7 @@ CREATE TABLE T_admin (
 	admin_password VARCHAR2(256) NOT NULL,
 	admin_name VARCHAR2(30) NOT NULL,
 	admin_email VARCHAR2(30),
-	admin_phone_numbr	VARCHAR2(11) NOT NULL,
+	admin_phone_numbr VARCHAR2(11) NOT NULL,
 	admin_created_at DATE NOT NULL,
 	admin_last_accessed_at DATE	NOT NULL,
 	admin_last_accessed_ip VARCHAR2(16)	NOT NULL
--- src/main/webapp/SQL/T_orderedProduct.sql
+++ src/main/webapp/SQL/T_orderedProduct.sql
@@ -1,11 +1,11 @@
 -- 주문하다 Order_product 테이블
-create table T_ordered_product(
+CREATE TABLE T_ordered_product(
 	ordered_product_id NUMBER(9),												--주문하다식별자
-	ordered_product_qunatity NUMBER(4) not null,									-- 주문수량
-	ordered_product_order_Fk NUMBER(9),										    -- 주문외래키
-	ordered_product_product_Fk NUMBER(9),										-- 상품외래키
+	ordered_product_qunatity NUMBER(4) NOT NULL,								-- 주문수량
+	ordered_product_order_fk NUMBER(9),										    -- 주문외래키
+	ordered_product_product_fk NUMBER(9),										-- 상품외래키
 	
-	primary key(ordered_product_id),												-- 기본키
-	foreign key(ordered_product_order_fk) references T_order(order_id),          -- 주문외래
-	foreign key(ordered_product_product_fk) references T_product(product_id) 	-- 상품외래키
+	PRIMARY KEY(ordered_product_id),											-- 기본키
+	FOREIGN KEY(ordered_product_order_fk) REFERENCES T_order(order_id),         -- 주문외래
+	FOREIGN KEY(ordered_product_product_fk) REFERENCES T_product(product_id) 	-- 상품외래키
 );
