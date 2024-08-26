7190a18b1712bc6723e6485bef87b74acfe2a59f
vemm8ks2 <vemm8ks2@gmail.com>
2024-08-26 17:08:33 +0900

    feature/44 DAO에 DB 연결 메소드 추가 및 템플릿 추가

--- src/main/java/com/teamtwo/model/ProductDAO.java
+++ src/main/java/com/teamtwo/model/ProductDAO.java
@@ -1,29 +1,122 @@
 package com.teamtwo.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ProductDAO implements BaseDAO<ProductDTO> {
 
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	private static ProductDAO instance = null;
+	
+	private ProductDAO() {}
+	
+	public static ProductDAO getInstance() {
+		if (instance == null) 
+			instance = new ProductDAO();
+		
+		return instance;
+	}
+	
+	private void open() {
+		try {
+			Context initCtx = new InitialContext();
+			Context ctx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) ctx.lookup("jdbc/myoracle");
+
+			conn = ds.getConnection();
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void close() {
+		try {
+			if (rs != null) rs.close();
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public ProductDTO get(int id) {
-		return null;
+		ProductDTO dto = null;
+
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return dto;
 	}
 
 	@Override
 	public List<ProductDTO> getAll() {
-		return null;
+		List<ProductDTO> list = new ArrayList<>();
+
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return list;
 	}
 
 	@Override
 	public void save(ProductDTO dto) {
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
 	}
 
 	@Override
 	public void update(ProductDTO dto) {
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
 	}
 
 	@Override
 	public void delete(int id) {
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
 	}
 
 }
--- src/main/java/com/teamtwo/model/UserDAO.java
+++ src/main/java/com/teamtwo/model/UserDAO.java
@@ -1,29 +1,122 @@
 package com.teamtwo.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class UserDAO implements BaseDAO<UserDTO> {
 
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	private static UserDAO instance = null;
+	
+	private UserDAO() {}
+	
+	public static UserDAO getInstance() {
+		if (instance == null) 
+			instance = new UserDAO();
+		
+		return instance;
+	}
+	
+	private void open() {
+		try {
+			Context initCtx = new InitialContext();
+			Context ctx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) ctx.lookup("jdbc/myoracle");
+
+			conn = ds.getConnection();
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void close() {
+		try {
+			if (rs != null) rs.close();
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public UserDTO get(int id) {
-		return null;
+		UserDTO dto = null;
+
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return dto;
 	}
 
 	@Override
 	public List<UserDTO> getAll() {
-		return null;
+		List<UserDTO> list = new ArrayList<>();
+
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return list;
 	}
 
 	@Override
 	public void save(UserDTO dto) {
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
 	}
 
 	@Override
 	public void update(UserDTO dto) {
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
 	}
 
 	@Override
 	public void delete(int id) {
+		try {
+			open();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
 	}
 
 }
