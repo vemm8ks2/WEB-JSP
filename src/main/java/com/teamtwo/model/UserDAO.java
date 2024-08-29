0da75daf2754826587ba6aea24f148a667364607
CAD03 <CAD03@DESKTOP-36504KH>
2024-08-29 10:38:24 +0900

    d

--- src/main/java/com/teamtwo/model/UserDAO.java
+++ src/main/java/com/teamtwo/model/UserDAO.java
@@ -54,9 +54,24 @@ public class UserDAO implements BaseDAO<UserDTO> {
 	@Override
 	public UserDTO get(int id) {
 		UserDTO dto = null;
-
 		try {
 			open();
+			String sql = "select * from T_customer where customer_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			
+			if(rs.next()) {
+             dto.setUserId(rs.getInt("customer_id"));
+             dto.setUserLoginId(rs.getString("customer_login_id"));
+             dto.setUserPassword(rs.getString("customer_password"));
+             dto.setUserName(rs.getString("customer_name"));
+             dto.setUserEmail(rs.getString("customer_email"));
+             dto.setUserPhoneNumber(rs.getString("customer_phone_number"));
+             dto.setUserGender(rs.getString("customer_gender"));
+             dto.setUserBirth(rs.getString("customer_birth"));
+             dto.setUserCreatedAt(rs.getString("customer_created_at"));
+             dto.setUserUpdatedAt(rs.getString("customer_updated_at"));
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -70,9 +85,27 @@ public class UserDAO implements BaseDAO<UserDTO> {
 	@Override
 	public List<UserDTO> getAll() {
 		List<UserDTO> list = new ArrayList<>();
-
 		try {
 			open();
+			String sql = "select * from T_customer";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+              UserDTO dto = new UserDTO();
+              dto.setUserId(rs.getInt("customer_id"));
+              dto.setUserLoginId(rs.getString("customer_login_id"));
+              dto.setUserPassword(rs.getString("customer_password"));
+              dto.setUserName(rs.getString("customer_name"));
+              dto.setUserEmail(rs.getString("customer_email"));
+              dto.setUserPhoneNumber(rs.getString("customer_phone_number"));
+              dto.setUserGender(rs.getString("customer_gender"));
+              dto.setUserBirth(rs.getString("customer_birth"));
+              dto.setUserCreatedAt(rs.getString("customer_created_at"));
+              dto.setUserUpdatedAt(rs.getString("customer_updated_at"));
+              
+              list.add(dto);
+            }
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -87,6 +120,16 @@ public class UserDAO implements BaseDAO<UserDTO> {
 	public void save(UserDTO dto) {
 		try {
 			open();
+			String sql = "insert into T_customer values(?,?,?,?,?,?,?,?,sysdate,sysdate)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, dto.getUserId());
+			pstmt.setString(2, dto.getUserLoginId());
+			pstmt.setString(3, dto.getUserPassword());
+			pstmt.setString(4, dto.getUserName());
+			pstmt.setString(5, dto.getUserEmail());
+			pstmt.setString(6, dto.getUserPhoneNumber());
+			pstmt.setString(7, dto.getUserGender());
+			pstmt.setString(8, dto.getUserBirth());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -99,6 +142,26 @@ public class UserDAO implements BaseDAO<UserDTO> {
 	public void update(UserDTO dto) {
 		try {
 			open();
+			String sql = "select * from T_customer where customer_id=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, dto.getUserId());
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+			  if(dto.getUserPassword().equals(rs.getString("customer_password"))) {
+			    sql = "update T_customer set customer_password=?, customer_name=?,"
+			        + "customer_email=?, customer_phone_number=?,"
+			        + "customer_birth=?, customer_updated_at=sysdate";
+			    pstmt = conn.prepareStatement(sql);
+			    
+			    pstmt.setString(1, dto.getUserPassword());
+			    pstmt.setString(2, dto.getUserName());
+			    pstmt.setString(3, dto.getUserEmail());
+			    pstmt.setString(4, dto.getUserPhoneNumber());
+			    pstmt.setString(5, dto.getUserBirth());
+			    
+			  }
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -111,6 +174,16 @@ public class UserDAO implements BaseDAO<UserDTO> {
 	public void delete(int id) {
 		try {
 			open();
+			String sql = "select * from T_customer where customer_id =?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+			  sql = "dalete from T_customer where customer_id=?";
+			  pstmt = conn.prepareStatement(sql);
+			  pstmt.setInt(1, id);
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
