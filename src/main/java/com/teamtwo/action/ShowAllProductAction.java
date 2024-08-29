c9f7df2b43ef6a64ff80cfebb61d68b977cd0d9b
CAD02 <CAD02@DESKTOP-ITDC4K5>
2024-08-29 17:40:39 +0900

    feature/88: 상품 전체 목록 보기 action 구현

---
+++ src/main/java/com/teamtwo/action/ShowAllProductAction.java
@@ -0,0 +1,34 @@
+package com.teamtwo.action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.teamtwo.model.ProductDAO;
+import com.teamtwo.model.ProductDTO;
+
+public class ShowAllProductAction implements Action {
+
+  @Override
+  public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    
+    ProductDAO dao = ProductDAO.getInstance();
+    
+    List<ProductDTO> list = dao.getAll();
+    
+    request.setAttribute("productList", list);
+    
+    ActionForward forward = new ActionForward();
+    
+    forward.setRedirect(false);
+    
+    forward.setPath("view/product_list.jsp");
+    
+    return forward;
+    
+  }
+
+}
