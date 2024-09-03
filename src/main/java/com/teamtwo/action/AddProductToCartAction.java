a1d9166d01ba4c8bf02382b52a6f02a812c1b028
vemm8ks2 <vemm8ks2@gmail.com>
2024-09-03 21:32:41 +0900

    docs: TODO, 작성자 주석 추가

--- src/main/java/com/teamtwo/action/AddProductToCartAction.java
+++ src/main/java/com/teamtwo/action/AddProductToCartAction.java
@@ -5,35 +5,42 @@ import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.teamtwo.model.CartDAO;
 import com.teamtwo.model.CartDTO;
 
+/**
+ * 장바구니에 상품을 추가하는 Action 입니다.
+ * 
+ * @author skwns0472
+ */
 public class AddProductToCartAction implements Action {
 
   @Override
   public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    
+
     int cart_product_count = Integer.parseInt(request.getParameter("cart_product_count").trim());
-    
+    /**
+     * Q(24.09.03): 유저 외래키는 세션에서 가져올 수 있음을 고려해보자.
+     */
     int cart_customer_fk = Integer.parseInt(request.getParameter("cart_customer_fk").trim());
-    
     int cart_product_fk = Integer.parseInt(request.getParameter("cart_product_fk").trim());
-    
+
+    CartDAO dao = CartDAO.getInstance();
+
     CartDTO dto = new CartDTO();
 
+    dto.setCartId(dao.getCartId());
     dto.setCartProductCount(cart_product_count);
     dto.setCartCustomerFk(cart_customer_fk);
     dto.setCartProductFk(cart_product_fk);
-    
-    CartDAO dao = CartDAO.getInstance();
-    
+
     dao.save(dto);
-    
+
     PrintWriter out = response.getWriter();
-    
     out.print("장바구니 상품 추가 성공");
-    
+
     return null;
   }
 
