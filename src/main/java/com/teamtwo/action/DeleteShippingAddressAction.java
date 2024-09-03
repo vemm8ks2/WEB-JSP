e0316819436844928773cf7a784eeb83f9fa4046
vemm8ks2 <vemm8ks2@gmail.com>
2024-09-03 17:53:40 +0900

    docs: 주석 추가
    TODO, 작성자 추가
    Close #118

--- src/main/java/com/teamtwo/action/DeleteShippingAddressAction.java
+++ src/main/java/com/teamtwo/action/DeleteShippingAddressAction.java
@@ -6,24 +6,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.teamtwo.model.ShippingAddressDAO;
 
+/**
+ * 배송지를 삭제하는 액션입니다.
+ * 
+ * @author hsheeh
+ */
 public class DeleteShippingAddressAction implements Action {
 
   @Override
   public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    
+
     int shippingAddressId = Integer.parseInt(request.getParameter("shippingAddressId").trim());
-    
+
     ShippingAddressDAO dao = ShippingAddressDAO.getInstance();
-    
+
+    /**
+     * TODO(24.09.03): 에러 핸들링
+     */
     dao.delete(shippingAddressId);
-    
+
     ActionForward forward = new ActionForward();
-    
+
     forward.setRedirect(false);
-    
     forward.setPath("");
-    
+
     return forward;
   }
 
