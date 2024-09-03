a79632e3a65d74364599066cd5c73fc3c9bfde7d
vemm8ks2 <vemm8ks2@gmail.com>
2024-09-03 21:18:07 +0900

    docs: 주석 추가
    작성자, TODO 주석 추가 및 SQL 수정
    Close #126

--- src/main/java/com/teamtwo/action/CreateOrderedProductAction.java
+++ src/main/java/com/teamtwo/action/CreateOrderedProductAction.java
@@ -7,18 +7,25 @@ import javax.servlet.http.HttpServletResponse;
 import com.teamtwo.model.OrderedProductDAO;
 import com.teamtwo.model.OrderedProductDTO;
 
+/**
+ * ERD 설계에 의해 생성된 OrderedProduct 테이블 생성 Action 입니다.
+ * 
+ * @author hsheeh
+ */
 public class CreateOrderedProductAction implements Action {
 
   @Override
   public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
     
+    /**
+     * TODO(24.09.03): orderedProductId 는 파라미터로 받는 게 아니고 dao 메소드를 호출하여 생성한다.
+     */
     int orderedProductId = Integer.parseInt(request.getParameter("orderedProductId").trim());
     int orderedProductQunatity = Integer.parseInt(request.getParameter("orderedProductQunatity").trim());
     int orderedProductUserIdFk = Integer.parseInt(request.getParameter("orderedProductUserIdFk").trim());
     int orderedProductProdIdFk = Integer.parseInt(request.getParameter("orderedProductProdIdFk").trim());
 
-    OrderedProductDAO dao = OrderedProductDAO.getInstance();
     OrderedProductDTO dto = new OrderedProductDTO();
     
     dto.setOrderedProductId(orderedProductId);
@@ -26,8 +33,9 @@ public class CreateOrderedProductAction implements Action {
     dto.setOrderedProductQunatity(orderedProductQunatity);
     dto.setOrderedProductUserIdFk(orderedProductUserIdFk);
     
-    dao.save(dto);
+    OrderedProductDAO dao = OrderedProductDAO.getInstance();
     
+    dao.save(dto);
     
     return null;
   }
