241d9e4f7ee1d66957c02b645e144c5586f59bc5
CAD03 <CAD03@DESKTOP-36504KH>
2024-09-03 14:31:57 +0900

    ModifyShippingAddress

---
+++ src/main/java/com/teamtwo/action/ModifyShippingAdddress.java
@@ -0,0 +1,34 @@
+package com.teamtwo.action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.teamtwo.model.ShippingAddressDAO;
+import com.teamtwo.model.ShippingAddressDTO;
+
+public class ModifyShippingAdddress implements Action {
+
+  @Override
+  public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    String shippingAddressAddress = request.getParameter("shippingAddressAddress").trim();;
+    String shippingAddressIsDefault = request.getParameter("shippingAddressIsDefault").trim();;
+    int shippingAddressId = Integer.parseInt(request.getParameter("shippingAddressId").trim());
+    int shippingAddressUserIdFk = Integer.parseInt(request.getParameter("shippingAddressUserIdFk").trim());
+
+    ShippingAddressDTO dto = new ShippingAddressDTO();
+
+    dto.setShippingAddressAddress(shippingAddressAddress);
+    dto.setShippingAddressId(shippingAddressId);
+    dto.setShippingAddressIsDefault(shippingAddressIsDefault);
+    dto.setShippingAddressUserIdFk(shippingAddressUserIdFk);
+
+    ShippingAddressDAO dao = ShippingAddressDAO.getInstance();
+    dao.update(dto);
+
+    return null;
+  }
+
+}
