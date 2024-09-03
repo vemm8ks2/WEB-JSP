a7ae5b4c054760414a7fa04fc2d2271653906d40
vemm8ks2 <vemm8ks2@gmail.com>
2024-09-03 21:36:36 +0900

    docs: 작성자, Q 주석 추가

--- src/main/java/com/teamtwo/action/LogoutAction.java
+++ src/main/java/com/teamtwo/action/LogoutAction.java
@@ -6,7 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * 로그아웃 Action 입니다.
+ * 
+ * @author skwns0472
+ */
 public class LogoutAction implements Action {
+  /**
+   * Q(24.09.03): 유저와 어드민 로그아웃을 같은 액션으로 혼용해도 되는가? 고민해보기.
+   */
 
   @Override
   public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
@@ -19,9 +27,7 @@ public class LogoutAction implements Action {
     ActionForward forward = new ActionForward();
     
     forward.setRedirect(false);
-    
     forward.setPath("main.jsp");
-    
     
     return forward;
   }
