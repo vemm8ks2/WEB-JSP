2838240e0b3e6e1232a3db2076f12f3035ba42cd
CAD03 <CAD03@DESKTOP-36504KH>
2024-09-03 18:26:27 +0900

    SearchProductByKeywardAction ProductDAO + search()

---
+++ src/main/java/com/teamtwo/action/SearchProductByKeywardAction.java
@@ -0,0 +1,29 @@
+package com.teamtwo.action;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.teamtwo.model.ProductDAO;
+import com.teamtwo.model.ProductDTO;
+
+public class SearchProductByKeywardAction implements Action {
+
+  @Override
+  public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    String productName = request.getParameter("productName").trim();
+
+    ProductDAO dao = ProductDAO.getInstance();
+    List<ProductDTO> list = dao.search(productName);
+
+    request.setAttribute("searchList", list);
+
+    ActionForward forward = new ActionForward();
+    forward.setRedirect(false);
+    forward.setPath("");
+    return forward;
+  }
+}
