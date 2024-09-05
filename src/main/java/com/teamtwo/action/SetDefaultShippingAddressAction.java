08c1a64cd96ffa0b8336ae136a667081ab934153
vemm8ks2 <vemm8ks2@gmail.com>
2024-09-05 12:17:37 +0900

    docs: javadoc 추가
    javadoc 추가, SQL 쿼리문 예약어 소문자를 대문자로 변경
    Close #148

--- src/main/java/com/teamtwo/action/SetDefaultShippingAddressAction.java
+++ src/main/java/com/teamtwo/action/SetDefaultShippingAddressAction.java
@@ -5,25 +5,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.teamtwo.model.ShippingAddressDAO;
-import com.teamtwo.model.ShippingAddressDTO;
 
+/**
+ * 고객의 기본 배송지를 설정하는 Action 입니다.
+ * 
+ * @author joohp
+ */
 public class SetDefaultShippingAddressAction implements Action {
 
   @Override
   public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    
-    int shipping_address_id = Integer.parseInt(request.getParameter("shipping_address_id").trim());
-    
+
+    int shippingAddressId = Integer.parseInt(request.getParameter("shipping_address_id").trim());
+
     ShippingAddressDAO dao = ShippingAddressDAO.getInstance();
-    
-    dao.updateDefault(shipping_address_id);
-    
+
+    /**
+     * TODO(24.09.05): 배송지를 새롭게 'Y'로 업데이트 해줄 뿐만 아니라 기존에 있던 기본 배송지도 'N'으로 업데이트해야 한다.
+     */
+    dao.updateDefault(shippingAddressId);
+
     ActionForward forword = new ActionForward();
-    
+
     forword.setRedirect(false);
     forword.setPath("");
-    
+
     return forword;
   }
 
