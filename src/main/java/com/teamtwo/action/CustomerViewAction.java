e52997ec5a7376657953551ee728b47b889145a1
vemm8ks2 <vemm8ks2@gmail.com>
2024-09-12 09:48:37 +0900

    feat: 세션 사용

--- src/main/java/com/teamtwo/action/CustomerViewAction.java
+++ src/main/java/com/teamtwo/action/CustomerViewAction.java
@@ -6,48 +6,52 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.teamtwo.model.CustomerDAO;
+import javax.servlet.http.HttpSession;
 import com.teamtwo.model.CustomerDTO;
 import com.teamtwo.model.ShippingAddressDAO;
 import com.teamtwo.model.ShippingAddressDTO;
 
+/**
+ * 유저의 개인 페이지를 보여주는 액션입니다.
+ * 
+ * @author skwns0472
+ */
 public class CustomerViewAction implements Action {
 
-	@Override
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+  @Override
+  public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
 
-		int customer_Id = Integer.parseInt(request.getParameter("id").trim());
-		
-		CustomerDAO dao = CustomerDAO.getInstance();
-		
-		ShippingAddressDAO shipping = ShippingAddressDAO.getInstance();
+    HttpSession session = request.getSession();
+    CustomerDTO customer = (CustomerDTO) session.getAttribute("customer"); // 세션에서 값 받아옴
 
-		
-		CustomerDTO page = dao.get(customer_Id);
-		
-		List<ShippingAddressDTO> list = shipping.customerAddress(customer_Id);		
-		
-		ShippingAddressDTO address = null;
-		
-		for(ShippingAddressDTO dto : list) {
-			if(dto.getShippingAddressIsDefault().equals("Y")) {
-				address = dto;
-			}
-		}
-		
-		request.setAttribute("customer", page);
-		request.setAttribute("address", address);
-		request.setAttribute("url", "customer.jsp");
-        request.setAttribute("stylesheet", "customer.css");
-		
-		ActionForward forward = new ActionForward();
-		
-		forward.setRedirect(false);
-		forward.setPath("view/layout.jsp");
-		
-		return forward;
-	}
+    ShippingAddressDAO shippingAddressDao = ShippingAddressDAO.getInstance();
+
+    List<ShippingAddressDTO> shippingAddressList =
+        shippingAddressDao.getShippingAddressByCustomer(customer.getCustomerId());
+
+    ShippingAddressDTO defaultAddress = null;
+
+    for (ShippingAddressDTO shippingAddress : shippingAddressList) {
+      boolean isDefaultAddress = shippingAddress.getShippingAddressIsDefault().equals("Y");
+
+      if (isDefaultAddress) {
+        defaultAddress = shippingAddress;
+        break;
+      }
+    }
+
+    request.setAttribute("customer", customer);
+    request.setAttribute("defaultAddress", defaultAddress);
+    request.setAttribute("url", "customer.jsp");
+    request.setAttribute("stylesheet", "customer.css");
+
+    ActionForward forward = new ActionForward();
+
+    forward.setRedirect(false);
+    forward.setPath("view/layout.jsp");
+
+    return forward;
+  }
 
 }
--- src/main/webapp/view/customer.jsp
+++ src/main/webapp/view/customer.jsp
@@ -1,26 +1,24 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <div class="customer-wrapper">
   <div class="customer-wrapper__image">
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <circle cx="12" cy="9" r="3" />
       <circle cx="12" cy="12" r="10" />
-      <path
-        d="M17.9691 20C17.81 17.1085 16.9247 15 11.9999 15C7.07521 15 6.18991 17.1085 6.03076 20"
-      />
+      <path d="M17.9691 20C17.81 17.1085 16.9247 15 11.9999 15C7.07521 15 6.18991 17.1085 6.03076 20" />
     </svg>
   </div>
-  <c:set var="dto" value="${customer}"/>
   <div>
-    <p class="customer-wrapper__name">${dto.getCustomerName() }</p>
-    <p>${dto.getCustomerPhoneNumber() }</p>
+    <p class="customer-wrapper__name">${customer.getCustomerName() }</p>
+    <p>${customer.getCustomerPhoneNumber() }</p>
   </div>
   <hr />
   <div>
-  <c:set var="ddto" value="${address}"/>
     <p>기본 배송지</p>
-    <p class="customer-wrapper__address">${ddto.getShippingAddressDestination()}</p>
+    <p class="customer-wrapper__address">
+      ${defaultAddress.getShippingAddressDestination()}
+    </p>
   </div>
   <hr />
   <div>
